df07097d2ab443d6515a0e3bc2ae30e88a80551b
Hüseyin Yontar <120130524+HuseyinYontar@users.noreply.github.com>
2025-12-20 20:05:34 +0300

    SA_DCT folder added

---
+++ SA_DCT/Contents.m
@@ -0,0 +1,38 @@
+% Anisotropic Nonparametric Image Restoration DemoBox (for Matlab 7.5 or later)
+% Version 1.22, July 6th 2023 (compatibility fix of v 1.20 of 9 May 2016)
+% Tampere University of Technology
+%
+%
+% Image Denoising.
+%   demo_DenoisingGaussian              - Anisotropic LPA-ICI Denoising demo (AWGN).
+%   demo_RecursiveDenoisingGaussian     - Recursive Anisotropic LPA-ICI Denoising demo (AWGN).
+%   demo_DenoisingSignDepNoise          - Anisotropic LPA-ICI Denoising demo (Signal-Dependent Noise).
+%   demo_MR_FilteringGaussian           - Anisotropic Multiresolution LPA denoising demo (AWGN).
+%
+% Inverse Filtering.
+%   demo_DeblurringGaussian             - Anisotropic LPA-ICI Regularized Deconvolution (AWGN).
+%   demo_DeblurringPoisson              - Anisotropic LPA-ICI Regularized Deconvolution (Poissonian Noise).
+%   demo_InverseHalftoning              - Anisotropic LPA-ICI Inverse Halftoning (Error-diffusion).
+%
+% Differentiation.
+%   demo_AnisotropicGradient            - Anisotropic Gradient demo (Riemann Surface).
+%
+% Adaptive Scale Selection.
+%   function_ICI                        - Intersection of Confidence Intervals (ICI) algorithm.
+%
+% Kernel design.
+%   demo_CreateLPAKernels               - Creates some LPA kernels.
+%   demo_CreateMRLPAKernels             - Creates and displays some Multiresolution LPA kernels.
+%   function_CreateLPAKernels           - Creates LPA kernels.
+%
+% Visualization.
+%   function_AnisSect_explorer          - Displays currently used anisotropic estimation neighborhoods
+%   utility_DrawLPAKernels              - Displays currently used LPA kernels.
+%
+% Other functions.
+%   function_Errors                     - Computes Error-criteria (SNR, MSE, etc.).
+%   function_stdEst                     - Estimates noise standard deviation (AWGN).
+%   function_ErrorDiffusion             - Generates Error-diffusion Halftones (e.g. Floyd-Steinberg, Jarvis).
+
+% Copyright Tampere University of Technology, Tampere, Finland
+% 
